f9621b3dd5b26a9c5259990ff60045748cf157a1
John Bowser <john.bow7720@gmail.com>
2020-08-30 16:58:02 -0600

    initial commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://john_bowser@localhost/disc_caddy',
+  JWT_SECRET: process.env.JWT_TOKEN || 'change-this-secret'
+}
